d169f5a9deb2ce6efac05973ed72a88756287702
MrPolymath <danielcarmona55@gmail.com>
2025-01-03 19:15:05 +0100

    Add support for opengraph image

--- website/src/app/post/[x_post_id]/metadata.ts
+++ website/src/app/post/[x_post_id]/metadata.ts
@@ -37,12 +37,21 @@ export async function generateMetadata({
       siteName: "Elongatd",
       type: "article",
       authors: [thread.author_name],
+      images: [
+        {
+          url: `https://elongatd.com/post/${params.x_post_id}/opengraph-image`,
+          width: 1200,
+          height: 630,
+          alt: `Thread by ${thread.author_name}`,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: `${thread.author_name} on Elongatd`,
       description,
       creator: `@${thread.author_username}`,
+      images: [`https://elongatd.com/post/${params.x_post_id}/opengraph-image`],
     },
   };
 }
